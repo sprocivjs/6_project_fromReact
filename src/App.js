60c1fa93a06241010751bd5e8fb722101e858290
sprocivjs <sprocivjs@gmail.com>
2022-09-15 14:03:57 +0300

    counter updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './index.scss';
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
-    </div>
-  );
+
+    const [count, setCount] = useState(0);
+
+    const onClickPlus = () => setCount(count + 1);
+    const onClickMinus = () => setCount(count - 1);
+    const onClickReset = () =>setCount(0);
+    const onClickRandom = () =>setCount(Math.floor(Math.random()*100));
+
+    return (
+        <div className="App">
+            <div>
+                <h2>Counter on the React:</h2>
+                <h1>{count}</h1>
+                <button onClick={onClickMinus} className="minus">- Minus</button>
+                <button onClick={onClickReset} className="reset">Reset</button>
+                <button onClick={onClickRandom} className="random">Random</button>
+                <button onClick={onClickPlus} className="plus">Plus +</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
